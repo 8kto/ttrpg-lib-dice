92624cce770776a14a78cce5f6e46a9f5d19e72a
8kto <ifhtagn@gmail.com>
2025-08-30 13:55:23 +0200

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { roll, rollDiceFormula, isValidDiceFormula } from './dice'
+export type { DiceRollResults, DiceRoll } from './dice'
 
 export { getRandomArrayItem, getRandomArrayItems, shuffleArray, secureRandomInteger } from './random'
 
