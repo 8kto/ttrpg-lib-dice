9a69ef8cc53d3f86cd18be4f889e0e4d27fdbb2a
8kto <ifhtagn@gmail.com>
2025-08-30 12:07:46 +0200

    Remove old approach, share the util call

--- src/dice.ts
+++ src/dice.ts
@@ -1,90 +1,6 @@
 import { Dice } from './domain/Dice'
 import { secureRandomInteger } from './random'
 
-/** @deprecated */
-type DiceOperationFn = (a: number, b: number) => number
-
-/** @deprecated */
-type Operator = '+' | '-'
-
-/** @deprecated */
-const Operations: Record<Operator, DiceOperationFn> = {
-  '+': (a: number, b: number): number => a + b,
-  '-': (a: number, b: number): number => a - b,
-}
-
-const operationRegExp = /([-+])/
-
-export const roll = (dice = Dice.d100): number => secureRandomInteger(1, dice)
-
-const isDiceRoll = (formula: string): boolean => /^\d*d\d+$/.test(formula.trim())
-
-const isInteger = (formula: string): boolean => /^[+-]?\d+$/.test(formula.trim())
-
-const rollNumberOfDice = (formula: string): number => {
-  const [numDice, numSides] = formula.split('d').map(Number)
-
-  if (isNaN(numDice) || isNaN(numSides)) {
-    throw new Error('Invalid dice formula')
-  }
-
-  let total = 0
-  let i = 0
-  do {
-    total += roll(numSides)
-  } while (++i < numDice)
-
-  return total
-}
-
-const getTokens__OLD = (formula: string): string[] => formula.split(operationRegExp)
-
-const resolveToken__OLD = (token: string): number | DiceOperationFn => {
-  if (isDiceRoll(token)) {
-    return rollNumberOfDice(token)
-  }
-  if (isInteger(token)) {
-    return parseInt(token, 10)
-  }
-  if (token in Operations) {
-    return Operations[token as Operator]
-  }
-
-  throw new Error(`Invalid token: ${token}`)
-}
-
-const getTokens = (formula: string): string[] =>
-  formula.match(/[+-]?\s*(?:\d*\s*d\s*\d+|\d+)/g)?.map((t) => t.replace(/\s+/g, '')) ?? []
-
-export const isValidDiceFormula = (formula: string): boolean => {
-  const diceRollPattern = /^(([1-9]\d*)?d[1-9]\d*|\d+)(\s*[-+]\s*((([1-9]\d*)?d[1-9]\d*)|\d+))*\s*$/
-  return diceRollPattern.test(formula.trim())
-}
-
-export const rollDiceFormula = (formula: string): number => {
-  if (!isValidDiceFormula(formula)) {
-    throw new Error(`Invalid dice formula, allowed characters are +-, numbers and dices (d6 etc.): ${formula}`)
-  }
-
-  const tokens = getTokens__OLD(formula).map(resolveToken__OLD)
-
-  let total = tokens[0] as number
-
-  for (let i = 1; i < tokens.length; i += 2) {
-    const operation = tokens[i] as DiceOperationFn
-    const value = tokens[i + 1] as number
-
-    if (!Number.isInteger(value) || !Number.isInteger(total) || typeof operation !== 'function') {
-      console.error({ operation, tokens, total, value })
-      throw new Error('Logic error, cannot parse tokens')
-    }
-
-    total = operation(total, value)
-  }
-
-  return total
-}
-
 export type DiceRoll = {
   formula: string
   total: number
@@ -95,6 +11,18 @@ export type DiceRollResults = {
   formula: string
   total: number
   rolls: DiceRoll[]
+}
+
+export const roll = (dice = Dice.d100): number => secureRandomInteger(1, dice)
+
+const isInteger = (formula: string): boolean => /^[+-]?\d+$/.test(formula.trim())
+
+const getTokens = (formula: string): string[] =>
+  formula.match(/[+-]?\s*(?:\d*\s*d\s*\d+|\d+)/g)?.map((t) => t.replace(/\s+/g, '')) ?? []
+
+export const isValidDiceFormula = (formula: string): boolean => {
+  const diceRollPattern = /^(([1-9]\d*)?d[1-9]\d*|\d+)(\s*[-+]\s*((([1-9]\d*)?d[1-9]\d*)|\d+))*\s*$/
+  return diceRollPattern.test(formula.trim())
 }
 
 export const rollDiceFormulaDetailed = (formula: string): DiceRollResults => {
@@ -161,4 +89,8 @@ export const rollDiceFormulaDetailed = (formula: string): DiceRollResults => {
   }
 
   return res
+}
+
+export const rollDiceFormula = (formula: string): number => {
+  return rollDiceFormulaDetailed(formula).total
 }
